43d0fc1dd4642a4fe970e1206f359999cd1f3d93
Ismail Colak <ismail.colak@ogr.iuc.edu.tr>
2024-07-16 19:39:22 +0300

    linuxgeçiş

--- cs/ConsoleApp1/Program.cs
+++ cs/ConsoleApp1/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Numerics;
-public class input{
+public class Input{
     public static void Main(){
         Console.WriteLine("Adinizi girin");
-        string ad=Console.ReadLine();
+        string? ad=Console.ReadLine();
         Console.WriteLine(ad);
     }
 }
